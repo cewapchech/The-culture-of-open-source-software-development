c51f859fc2883a8843f8b9b4fac22bd3bd1feaaa
cewapchech <asparrow2007@gmail.com>
2026-02-25 16:39:40 +0800

    second cod

--- linux_as_execenv/pinger.py
+++ linux_as_execenv/pinger.py
@@ -1,4 +1,14 @@
 import requests
+from datetime import datetime, timedelta
+
+url_template = "hhtps://simurg.space/gen_file?data=obs&date={date}"
+now=datetime.now()
+date=now.strftime("%Y-%m-%d")
+url = url_template.format(date=date)
+
+response = requests.get(url=url, stream=True)
+print(f"For{date} got: ", response)
+import requests
 from datetime import datetime
 
 url_template = "hhtps://simurg.space/gen_file?data=obs&date={date}"
